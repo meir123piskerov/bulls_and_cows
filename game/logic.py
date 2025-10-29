7bbdfa3197457a6acdb2f09ecc1e1a09883a66fd
meir_p <meir12345p@gmail.com>
2025-10-29 12:25:44 +0200

    score_guess

---
+++ game/logic.py
@@ -0,0 +1,15 @@
+def score_guess(secret: str, guess: str) -> tuple[int, int]:
+    number_1 = secret
+    user_guess = guess
+    good = 0
+    correct = 0
+    for number in range(len(number_1)):
+        if number_1[number] == user_guess[number]:
+            good += 1
+    for number in range(len(number_1)):
+        if number_1[number] != user_guess[number] and user_guess[number] in number_1:
+            correct+=1
+    return good , correct
+
+
+
